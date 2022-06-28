06b3af41a995ab14dd874b915b7682da9bcccd41
stavgafny <stavgafny@gmail.com>
2022-06-28 20:12:01 +0300

    first commit (initialized project)

---
+++ android/app/src/main/kotlin/com/example/lifeline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lifeline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lifeline/constants/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  //!ROOT
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const Scaffold(),
+    );
+  }
+}
