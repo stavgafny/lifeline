88572f807ae3c3be8ad1326a62786c08e177a0a8
stavgafny <stavgafny@gmail.com>
2022-11-13 16:35:51 +0200

    tweaked dark theme colors

--- lib/constants/theme_data.dart
+++ lib/constants/theme_data.dart
@@ -39,15 +39,15 @@ ThemeData darkTheme = ThemeData(
   useMaterial3: true,
   colorScheme: const ColorScheme(
     brightness: Brightness.dark,
-    primary: Color(0xFF9B79E1),
-    onPrimary: Color(0xFF34304B),
-    secondary: Color(0xFF151219),
+    primary: Color(0xFFFDB2E8),
+    onPrimary: Color(0xFF33242F),
+    secondary: Color(0xFF221B23),
     onSecondary: Color(0xFF655E6A),
     error: Colors.red,
     onError: Colors.red,
-    background: Color(0xFF332D47),
+    background: Color(0xFF472D3D),
     onBackground: Colors.black,
-    surface: Color(0xFF211D26),
+    surface: Color(0xFF362932),
     onSurface: Color(0xFFDDCDEE),
   ),
   textTheme: GoogleFonts.montserratTextTheme().apply(
@@ -56,10 +56,10 @@ ThemeData darkTheme = ThemeData(
   scaffoldBackgroundColor: const Color(0xFF1C171D),
   appBarTheme: const AppBarTheme(
     foregroundColor: Color(0xFFE9DDF7),
-    backgroundColor: Color(0xFF503B55),
+    backgroundColor: Color(0xFF553B54),
   ),
   navigationBarTheme: const NavigationBarThemeData(
-    backgroundColor: Color(0xFF64476B),
+    backgroundColor: Color(0xFF6B4765),
     indicatorColor: Color(0xFFE9DDF7),
   ),
   snackBarTheme: const SnackBarThemeData(
