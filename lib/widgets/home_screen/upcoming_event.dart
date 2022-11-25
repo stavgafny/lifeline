8f64b6c541611b87d520e74d633d0b7ecb87e3d6
stavgafny <stavgafny@gmail.com>
2022-11-25 22:52:55 +0200

    upcoming events responsive dimensions

--- lib/widgets/home_screen/upcoming_event.dart
+++ lib/widgets/home_screen/upcoming_event.dart
@@ -1,65 +1,103 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../models/upcoming_event_model.dart';
 
 class UpcomingEvent extends StatelessWidget {
+  static const double _dateFontSize = 14.0;
+  static const double _nameFontSize = 20.0;
+
+  static const int defaultMinimumSize = 125;
+
+  static double get additionalTextSizes => _dateFontSize + _nameFontSize;
+
   static Widget addButton(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        shape: BoxShape.circle,
-      ),
-      child: Transform.scale(
-        scale: .5,
-        child: const FittedBox(
-          fit: BoxFit.contain,
-          child: Icon(
-            Icons.add,
+    return Column(
+      children: [
+        const SizedBox(height: _dateFontSize),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              shape: BoxShape.circle,
+            ),
+            child: Transform.scale(
+              scale: .5,
+              child: const FittedBox(
+                fit: BoxFit.contain,
+                child: Icon(
+                  Icons.add,
+                ),
+              ),
+            ),
           ),
         ),
-      ),
+        const SizedBox(height: _nameFontSize),
+      ],
     );
   }
 
   final UpcomingEventModel model;
-
   const UpcomingEvent({
     required this.model,
     Key? key,
   }) : super(key: key);
 
   Widget _buildDate(BuildContext context) {
-    return const Text(
-      "12/1/2001",
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontSize: 10.0,
+    return const SizedBox(
+      height: _dateFontSize,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Text(
+          "12/1/2001",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildName(BuildContext context) {
+    return Center(
+      child: Text(
+        model.name,
+        style: const TextStyle(
+          fontSize: _nameFontSize,
+          height: 1.0,
+        ),
+        textAlign: TextAlign.center,
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
       ),
     );
   }
 
   Widget _buildBlob(BuildContext context) {
-    return Flexible(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary,
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            fit: BoxFit.contain,
-            image: model.type.value,
-            opacity: .65,
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.contain,
+              image: model.type.value,
+              opacity: .65,
+            ),
           ),
-        ),
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Transform.scale(
-            scale: .75,
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
             child: FittedBox(
               fit: BoxFit.contain,
               child: Text(
                 "13",
                 textAlign: TextAlign.center,
+                textHeightBehavior: const TextHeightBehavior(
+                  applyHeightToFirstAscent: true,
+                  applyHeightToLastDescent: false,
+                ),
                 style: GoogleFonts.pacifico(
                   color: Theme.of(context).colorScheme.onSurface,
                 ),
@@ -71,31 +109,14 @@ class UpcomingEvent extends StatelessWidget {
     );
   }
 
-  Widget _buildName(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Text(
-        model.name,
-        textAlign: TextAlign.center,
-        softWrap: true,
-        overflow: TextOverflow.ellipsis,
-        maxLines: 1,
-        textWidthBasis: TextWidthBasis.longestLine,
-        style: const TextStyle(fontSize: 18.0),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        children: [
-          _buildDate(context),
-          _buildBlob(context),
-          _buildName(context),
-        ],
-      ),
+    return Column(
+      children: [
+        _buildDate(context),
+        _buildBlob(context),
+        _buildName(context),
+      ],
     );
   }
 }
--- lib/widgets/home_screen/upcoming_events.dart
+++ lib/widgets/home_screen/upcoming_events.dart
@@ -12,7 +12,7 @@ class UpcomingEvents extends StatefulWidget {
 class _UpcomingEventsState extends State<UpcomingEvents> {
   final List<UpcomingEventModel> _upcomingEvents = [
     UpcomingEventModel(
-      name: "AAA",
+      name: "AAAasasd asdasd asdasd",
       date: DateTime.now(),
       type: UpcomingEventType.celebration,
     ),
@@ -30,42 +30,57 @@ class _UpcomingEventsState extends State<UpcomingEvents> {
 
   @override
   Widget build(BuildContext context) {
+    // Gets screen dimensions
     final screenSize = MediaQuery.of(context).size;
 
-    final height = screenSize.height / 5;
-    final width = screenSize.width / 3;
+    // Max height
+    final maxHeight = screenSize.height / 4.0;
 
-    return SizedBox(
-      width: double.infinity,
-      height: height,
-      child: Theme(
-        //! Remove highlight color on reorder drag
-        data: Theme.of(context).copyWith(
-          canvasColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-        ),
-        child: ReorderableListView(
-          scrollDirection: Axis.horizontal,
-          onReorder: (oldIndex, newIndex) {
-            if (newIndex > oldIndex) newIndex--;
-            final upcomingEvent = _upcomingEvents.removeAt(oldIndex);
-            _upcomingEvents.insert(newIndex, upcomingEvent);
-            setState(() {});
-          },
-          footer: SizedBox(
-            width: width,
-            child: UpcomingEvent.addButton(context),
+    // Number of upcoming events displayed at once
+    final displayedOnScreen =
+        screenSize.width ~/ UpcomingEvent.defaultMinimumSize;
+
+    // Final size of every upcoming event is the screen width divided by the number displayed at once
+    // If that number exceeds the total number of upcoming events then the size is the screen width divided by the number of upcoming events
+    final size = screenSize.width /
+        (displayedOnScreen > _upcomingEvents.length
+            ? _upcomingEvents.length
+            : displayedOnScreen);
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxHeight: maxHeight),
+      child: SizedBox(
+        width: double.infinity,
+        height: size + UpcomingEvent.additionalTextSizes,
+        child: Theme(
+          //! Remove highlight color on reorder drag
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.transparent,
+            shadowColor: Colors.transparent,
           ),
-          children: [
-            for (final upcomingEvent in _upcomingEvents)
-              SizedBox(
-                key: ValueKey(upcomingEvent),
-                width: width,
-                child: UpcomingEvent(
-                  model: upcomingEvent,
+          child: ReorderableListView(
+            scrollDirection: Axis.horizontal,
+            onReorder: (oldIndex, newIndex) {
+              if (newIndex > oldIndex) newIndex--;
+              final upcomingEvent = _upcomingEvents.removeAt(oldIndex);
+              _upcomingEvents.insert(newIndex, upcomingEvent);
+              setState(() {});
+            },
+            footer: SizedBox(
+              width: size,
+              child: UpcomingEvent.addButton(context),
+            ),
+            children: [
+              for (final upcomingEvent in _upcomingEvents)
+                SizedBox(
+                  key: ValueKey(upcomingEvent),
+                  width: size,
+                  child: UpcomingEvent(
+                    model: upcomingEvent,
+                  ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
