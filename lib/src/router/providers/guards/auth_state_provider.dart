61dd587a5494bec58d1199d73963647b84c39e7e
stavgafny <stavgafny@gmail.com>
2023-06-05 22:17:39 +0300

    preserving encapsulation on AuthState

--- lib/src/router/providers/guards/auth_state_provider.dart
+++ lib/src/router/providers/guards/auth_state_provider.dart
@@ -1,16 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final authProvider = StreamProvider<_UserAuthState>((ref) {
+final authStateProvider = StreamProvider<AuthState>((ref) {
   return FirebaseAuth.instance
       .authStateChanges()
-      .map((user) => _UserAuthState(user));
+      .map((user) => AuthState(user));
 });
 
-class _UserAuthState {
+class AuthState {
   final User? _user;
 
-  const _UserAuthState(this._user);
+  const AuthState(this._user);
 
   /// Whether the user exists - signed in (not necessarily verified)
   bool get exist => _user != null && _user?.uid != null;
--- lib/src/router/providers/router_provider.dart
+++ lib/src/router/providers/router_provider.dart
@@ -2,12 +2,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../routes/app_routes.dart';
-import './guards/auth_provider.dart';
+import './guards/auth_state_provider.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
 
 final routerProvider = Provider.autoDispose<GoRouter>((ref) {
-  final authState = ref.watch(authProvider);
+  final authState = ref.watch(authStateProvider);
 
   return GoRouter(
     navigatorKey: _rootNavigatorKey,
