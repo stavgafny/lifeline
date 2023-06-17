b33c2801077054d3c1f9d89b6d244afe4b542c4f
stavgafny <stavgafny@gmail.com>
2023-06-17 21:27:33 +0300

    fix: Incorrect use of ParentDataWidget

--- lib/src/features/authentication/shared/widgets/error_box.dart
+++ lib/src/features/authentication/shared/widgets/error_box.dart
@@ -6,35 +6,35 @@ class ErrorBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xFFFF6363),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Row(
-          children: [
-            const SizedBox(width: 10.0),
-            const Icon(
-              Icons.error_outline,
-              color: Color(0xFF373232),
-              size: 28.0,
-            ),
-            const SizedBox(width: 5),
-            Expanded(
-              child: Text(
-                message,
-                style: const TextStyle(
-                  color: Color(0xFF373232),
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFF6363),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        children: [
+          const SizedBox(width: 10.0),
+          const Icon(
+            Icons.error_outline,
+            color: Color(0xFF373232),
+            size: 28.0,
+          ),
+          const SizedBox(width: 5),
+          Expanded(
+            child: Text(
+              message,
+              style: const TextStyle(
+                color: Color(0xFF373232),
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold,
               ),
-            )
-          ],
-        ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          )
+        ],
       ),
     );
   }
