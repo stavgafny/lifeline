6190b28257f249a05dfa2779c03ad4ac0c2dd77a
stavgafny <stavgafny@gmail.com>
2023-06-28 00:03:47 +0300

    setting fixed loading sheet properties

--- lib/src/features/authentication/shared/widgets/loading_sheet.dart
+++ lib/src/features/authentication/shared/widgets/loading_sheet.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-int _i = 0;
-const _ani = [
-  LoadingAnimationWidget.fourRotatingDots,
-  LoadingAnimationWidget.staggeredDotsWave,
-  LoadingAnimationWidget.stretchedDots,
-  LoadingAnimationWidget.inkDrop
-];
+//! All fitting animations
+//? LoadingAnimationWidget.fourRotatingDots,
+//? LoadingAnimationWidget.staggeredDotsWave,
+//? LoadingAnimationWidget.stretchedDots,
+//? LoadingAnimationWidget.inkDrop
 
 class LoadingSheet extends StatelessWidget {
+  static const _animationSize = 80.0;
+  static const _sheetHeight = 250.0;
+  static const _sheetRadius = Radius.circular(50.0);
+
+  static final Widget _loadingAnimation =
+      LoadingAnimationWidget.fourRotatingDots(
+    color: const Color(0xFFEDD8FB),
+    size: _animationSize,
+  );
+
   static Future<void> show(BuildContext context) {
     return showModalBottomSheet(
       isDismissible: false,
       enableDrag: true,
       backgroundColor: Theme.of(context).colorScheme.background,
       shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(50.0)),
+        borderRadius: BorderRadius.vertical(top: _sheetRadius),
       ),
       context: context,
       builder: (_) => const LoadingSheet._(),
@@ -27,12 +35,10 @@ class LoadingSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _i = (_i + 1) % _ani.length;
     return SizedBox(
-      height: 250.0,
+      height: _sheetHeight,
       child: Center(
-        child:
-            _ani[_i](color: Theme.of(context).colorScheme.onSurface, size: 80),
+        child: _loadingAnimation,
       ),
     );
   }
