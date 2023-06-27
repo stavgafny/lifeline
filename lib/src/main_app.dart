a3b07a0869c75816d57de37dd9e672839cc1c233
stavgafny <stavgafny@gmail.com>
2023-06-27 23:35:25 +0300

    setting theme mode provider

--- lib/src/main_app.dart
+++ lib/src/main_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './constants/theme/app_theme.dart' as app_theme;
 import './router/providers/router_provider.dart';
+import './providers/theme_mode_provider.dart';
 
 class MainApp extends ConsumerWidget {
   static const title = "Lifeline";
@@ -11,12 +12,13 @@ class MainApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
+    final themeMode = ref.watch(themeModeProvider);
 
     return MaterialApp.router(
       title: title,
       theme: app_theme.lightTheme,
       darkTheme: app_theme.darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: themeMode,
       routerConfig: router,
     );
   }
---
+++ lib/src/providers/theme_mode_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final themeModeProvider = StateNotifierProvider<ThemeModeController, ThemeMode>(
+  // final brightness = MediaQuery.of(context).platformBrightness;
+  // final isDarkMode = brightness == Brightness.dark;
+  (ref) => ThemeModeController(ThemeMode.system),
+);
+
+class ThemeModeController extends StateNotifier<ThemeMode> {
+  ThemeModeController(super._state);
+
+  void setMode(ThemeMode mode) {
+    state = mode;
+  }
+
+  void toggleMode() {
+    state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  @override
+  String toString() {
+    switch (state) {
+      case ThemeMode.system:
+        return "System";
+      case ThemeMode.light:
+        return "Light";
+      case ThemeMode.dark:
+        return "Dark";
+    }
+  }
+}
