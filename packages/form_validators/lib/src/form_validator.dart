5fe31fd696eb88ac85883b050c174d6510796c50
stavgafny <stavgafny@gmail.com>
2023-07-02 14:41:20 +0300

    renaming form validator pure/dirty error flag

--- packages/form_validators/lib/src/form_validator.dart
+++ packages/form_validators/lib/src/form_validator.dart
@@ -2,12 +2,12 @@ enum FormSubmissionStatus { init, progress, failure, success }
 
 abstract class FormValidator<T, E extends Enum> {
   final T value;
-  final bool _needsToValidate;
+  final bool _errorPresent;
 
-  const FormValidator.pure(this.value) : _needsToValidate = false;
-  const FormValidator.dirty(this.value) : _needsToValidate = true;
+  const FormValidator.pure(this.value) : _errorPresent = false;
+  const FormValidator.dirty(this.value) : _errorPresent = true;
 
-  E? get error => _needsToValidate ? validate() : null;
+  E? get error => _errorPresent ? validate() : null;
 
   E? validate();
 
